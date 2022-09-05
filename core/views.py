a9577fedec301ef169bdab18a87f251b04252849
vegapunkk <senjuruben@gmail.com>
2022-09-05 16:14:49 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Categoria, Marca, Carro
+from core.serializers import CategoriaSerializer, CarroSerializer, MarcaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class CarroViewSet(ModelViewSet):
+    queryset = Carro.objects.all()
+    serializer_class = CarroSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
