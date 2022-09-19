82c0bbc9d7c486de37e5b2e03009e7cf9140fbe5
vegapunkk <senjuruben@gmail.com>
2022-09-19 14:59:30 -0300

    commit final

--- garagem/settings.py
+++ garagem/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
